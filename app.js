198ad37c07b86f6e7f7b54ac96b9af7f60f6ebc8
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-08-05 10:56:16 +0900

    Create fetchDom function

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 (function () {
+  function fetchDOM () {
+    const app = document.querySelector('#app');
+    const container = app.querySelector('.entry-container');
+    const loadMore = app.querySelector('.load-more');
+  }
   
 })();
