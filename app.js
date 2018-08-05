be3613849c616f18dcbeb0f7fb4c6886dd07f7cb
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-08-05 11:15:24 +0900

    Create getUsers function

--- app.js
+++ app.js
@@ -3,6 +3,18 @@
     const app = document.querySelector('#app');
     const container = app.querySelector('.entry-container');
     const loadMore = app.querySelector('.load-more');
+  };
+  
+  async function getPosts (page = 1) {
+    const result = await fetch('https://jsonplaceholder.typicode.com/posts?_page=' + page);
+    
+    return await result.json();
+  }
+  
+  async function getUsers () {
+    const result = await fetch('https://jsonplaceholder.typicode.com/users');
+    
+    return await result.json();
   }
   
 })();
