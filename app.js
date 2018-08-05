fb3201dd18893a6676fa6dba8744670514662ad4
Marcin Szczepanski <marcin.l.szczepanski@gmail.com>
2018-08-05 14:09:44 +0900

    Create loadEntries function

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 (function () {
-  function fetchDOM () {
     const app = document.querySelector('#app');
     const container = app.querySelector('.entry-container');
     const loadMore = app.querySelector('.load-more');
-  };
   
   async function getPosts (page = 1) {
     const result = await fetch('https://jsonplaceholder.typicode.com/posts?_page=' + page);
@@ -16,5 +14,19 @@
     
     return await result.json();
   }
+  
+  async function loadEntries (page = 1) {
+    const [posts, users] = await Promise.all([getPosts(page), getUsers()]);
+   // console.log(posts);
+  //  console.log(users);
+    
+      return posts.map(post => {
+    //    console.log(post);
+        const user = users.filter(u => u.id === post.userId)[0];
+    //    console.log(user);
+        return `<section class="entry"><h2 class="entry-title">${post.title}</h2><article class="entry-body">${post.body}</article><div class="entry-author"><a href="mailto:${user.email}">${user.name}</a></div></section>`;
+        }).join('');
+  }
+
   
 })();
